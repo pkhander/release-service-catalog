93d4a2e7ac422ae26f8ff7229a7be57acb384e19
pkhander <pkhander@redhat.com>
2024-05-28 19:22:48 -0400

    add publish-to-cgw task
    feat(CLOUDDST-22189): add publish-to-cgw task
    
    This commit creates a task that will publish content to Developer Portal
    
    Signed-off-by: Parthey Khanderia <pkhander@redhat.com>

---
+++ tasks/publish-to-cgw/tests/pre-apply-task-hook.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+TASK_PATH="$1"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Add mocks to the beginning of task step script
+yq -i '.spec.steps[0].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[0].script' "$TASK_PATH"
+
+# Create a dummy pyxis secret (and delete it first if it exists)
+kubectl delete secret test-publish-to-cgw-secret --ignore-not-found
+kubectl create secret generic test-publish-to-cgw-secret --from-literal=cert=mycert --from-literal=key=mykey
