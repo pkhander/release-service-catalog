6162202336c99fd0d8e98e1a05754c44dc137715
pkhander <pkhander@redhat.com>
2024-06-27 15:14:12 -0400

    working version

--- tasks/publish-to-cgw/tests/pre-apply-task-hook.sh
+++ tasks/publish-to-cgw/tests/pre-apply-task-hook.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+# Add mocks to the beginning of task step script
 TASK_PATH="$1"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-# Add mocks to the beginning of task step script
 yq -i '.spec.steps[0].script = load_str("'$SCRIPT_DIR'/mocks.sh") + .spec.steps[0].script' "$TASK_PATH"
 
-# Create a dummy pyxis secret (and delete it first if it exists)
+# Create a dummy exodus secret (and delete it first if it exists)
 kubectl delete secret test-publish-to-cgw-secret --ignore-not-found
-kubectl create secret generic test-publish-to-cgw-secret --from-literal=cert=mycert --from-literal=key=mykey
+kubectl create secret generic test-publish-to-cgw-secret --from-literal=username=mycert --from-literal=token=mykey
